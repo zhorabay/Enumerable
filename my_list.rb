65a4fb13c549fd4dc70b4a2edcf8082a93e6453f
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-11-01 16:23:21 +0200

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require_relative './MyEnumerable'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -15,12 +15,12 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
 # Test #filter
-puts list.filter(&:even?)
+puts(list.filter(&:even?))
